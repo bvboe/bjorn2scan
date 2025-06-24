8809c02c76cdc22ac4cf633649ccaef40e68ef4b
Bjorn Boe <bvboe@pobox.com>
2025-06-24 15:01:12 -0400

    Upgraded Kubernetes integration component and added optional SSL verification for MicroK8s.

--- pod-scanner/src/python/host_configuration.py
+++ pod-scanner/src/python/host_configuration.py
@@ -3,6 +3,7 @@ import os
 import json
 import subprocess
 from kubernetes import client, config, watch
+from kubernetes.client import Configuration
 
 logger = logging.getLogger(__name__)
 
@@ -10,12 +11,21 @@ NODE_NAME=os.getenv("NODE_NAME")
 NODE_IP=os.getenv("NODE_IP")
 POTENTIAL_DOCKER_SOCKET_LOCATIONS = os.getenv("POTENTIAL_DOCKER_SOCKET_LOCATIONS")
 POTENTIAL_CONTAINERD_SNAPSHOT_LOCATIONS = os.getenv("POTENTIAL_CONTAINERD_SNAPSHOT_LOCATIONS")
+K8S_VERIFY_SSL = os.getenv("K8S_VERIFY_SSL", "true").lower() in ("1", "true", "yes")
 logger.info(f"POTENTIAL_DOCKER_SOCKET_LOCATIONS {POTENTIAL_DOCKER_SOCKET_LOCATIONS}")
 logger.info(f"POTENTIAL_CONTAINERD_SNAPSHOT_LOCATIONS {POTENTIAL_CONTAINERD_SNAPSHOT_LOCATIONS}")
+logger.info(f"K8S_VERIFY_SSL {K8S_VERIFY_SSL}")
 
 def get_container_runtime():
     logger.debug("get_container_runtime()")
     config.load_incluster_config()
+
+    if K8S_VERIFY_SSL == False:
+        # Get the default Kubernetes configuration and disable SSL verification, to maintain compatibility with certain Kubernetes distributions, like MicroK8s
+        cfg = Configuration.get_default_copy()
+        cfg.verify_ssl = False
+        Configuration.set_default(cfg)
+
     v1 = client.CoreV1Api()
     nodes = v1.list_node()
 
